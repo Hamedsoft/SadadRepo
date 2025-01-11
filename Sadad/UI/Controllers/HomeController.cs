636e48aacd74bd0d7643cef257d956f1be80a4f7
Hamed Ziaei <h.zEEAEE@gmail.com>
2025-01-11 18:46:19 +0330

    [Fix] Change UsesUI Controler from services

--- Sadad/UI/Controllers/HomeController.cs
+++ Sadad/UI/Controllers/HomeController.cs
@@ -6,22 +6,21 @@ using System.Diagnostics;
 using System.Text;
 using UI.Models;
 using UI.Services;
+using API.Models;
 
 namespace UI.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-        private readonly HttpClient _httpClient;
         private readonly IOrderService _orderService;
-        INotyfService _NotyfService;
+        private readonly INotyfService _notyfService;
 
-        public HomeController(ILogger<HomeController> logger, HttpClient httpClient, INotyfService notyfService, IOrderService orderService)
+        public HomeController(ILogger<HomeController> logger, IOrderService orderService, INotyfService notyfService)
         {
             _logger = logger;
-            _httpClient = httpClient;
-            _NotyfService = notyfService;
             _orderService = orderService;
+            _notyfService = notyfService;
         }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
@@ -30,133 +29,82 @@ namespace UI.Controllers
         }
         public async Task<ActionResult> Products()
         {
-            var response = await _httpClient.GetAsync($"Orders/GetProducts");
-            if (!response.IsSuccessStatusCode)
-            {
-                return View("Error");
-            }
+            var products = await _orderService.GetProducts();
+            if (products == null || !products.Any()) return View("Error");
 
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            var data = JsonConvert.DeserializeObject<List<ProductDto>>(jsonResponse);
-            List<OrderItemDto> DraftOrderItems = await GetLastOpenOrderItems();
-            ViewData["OpenOrderCount"] = DraftOrderItems.Count();
-            return View(data);
+            var draftOrderItems = await _orderService.GetLastOpenOrderItems(1);
+            ViewData["OpenOrderCount"] = draftOrderItems.Count();
+            return View(products);
         }
-        private async Task<List<OrderItemDto>> GetLastOpenOrderItems(int CustomerId = 1)
+        //private async Task<List<OrderItemDto>> GetLastOpenOrderItems(int CustomerId = 1)
+        //{
+        //    List<OrderItemDto> Result = new List<OrderItemDto>();
+        //    var response = await _httpClient.GetAsync($"Orders/GetLastOpenOrderItems/{CustomerId}");
+        //    if (!response.IsSuccessStatusCode)
+        //    {
+        //        return Result;
+        //    }
+        //    var jsonResponse = await response.Content.ReadAsStringAsync();
+        //    Result = JsonConvert.DeserializeObject<List<OrderItemDto>>(jsonResponse);
+        //    return Result;
+        //}
+        public async Task<IActionResult> Orders(int customerId = 1)
         {
-            List<OrderItemDto> Result = new List<OrderItemDto>();
-            var response = await _httpClient.GetAsync($"Orders/GetLastOpenOrderItems/{CustomerId}");
-            if (!response.IsSuccessStatusCode)
+            var orders = await _orderService.GetOrderGroupItems(customerId);
+            if (!orders.Any())
             {
-                return Result;
-            }
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            Result = JsonConvert.DeserializeObject<List<OrderItemDto>>(jsonResponse);
-            return Result;
-        }
-        public async Task<IActionResult> Orders(int CustomerId = 1)
-        {
-            var response = await _httpClient.GetAsync($"Orders/GetOrderGroupItems/{CustomerId}");
-            if (!response.IsSuccessStatusCode)
-            {
-                return View("Error");
+                _notyfService.Error("سبد محصولات خالی است");
+                return RedirectToAction("Products");
             }
 
-            var jsonResponse = await response.Content.ReadAsStringAsync();
-            var Model = JsonConvert.DeserializeObject<List<OrderListGroupDto>>(jsonResponse);
-
-            if (Model != null && Model.Count() > 0)
-            {
-                ViewData["OrderCount"] = Model.Count();
-                ViewData["OrderSubTotal"] = Model.Sum(M => M.Total);
-                return View(Model);
-            }
-            else
-            {
-                _NotyfService.Error("سبد محصولات خالی است");
-                return Redirect("Products");
-            }
+            ViewData["OrderCount"] = orders.Count;
+            ViewData["OrderSubTotal"] = orders.Sum(m => m.Total);
+            return View(orders);
         }
         [HttpPost]
-        public async Task<IActionResult> CommitOrderAsync(int OrderId)
+        [HttpPost]
+        public async Task<IActionResult> CommitOrderAsync(int orderId)
         {
-            var CommOrderDto = new
+            var isSuccess = await _orderService.CommitOrderAsync(orderId);
+            if (!isSuccess)
             {
-                orderId = OrderId
-            };
-
-            var jsonData = JsonConvert.SerializeObject(CommOrderDto);
-            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            var request = new HttpRequestMessage(HttpMethod.Put, "Orders/CommitOrder")
-            {
-                Content = content
-            };
-
-            var response = await _httpClient.SendAsync(request);
-
-
-            if (!response.IsSuccessStatusCode)
-            {
-                _NotyfService.Error("خطا در ارسال درخواست");
+                _notyfService.Error("خطا در ارسال درخواست");
+                return View();
             }
 
-            var result = await response.Content.ReadAsStringAsync();
-            _NotyfService.Success("فرایند درخواست سفارش تکمیل شد");
-            ViewData["OrderId"] = OrderId;
+            _notyfService.Success("فرایند درخواست سفارش تکمیل شد");
+            ViewData["OrderId"] = orderId;
             return View();
         }
         [HttpPost]
-        public async Task<IActionResult> AddOrderItem(int CustomerId, int ProductId)
+        public async Task<IActionResult> AddOrderItem(int customerId, int productId)
         {
-            var orderItemData = new
+            var isSuccess = await _orderService.AddOrderItem(customerId, productId);
+            if (!isSuccess)
             {
-                customerId = CustomerId,
-                productId = ProductId
-            };
-
-            var jsonData = JsonConvert.SerializeObject(orderItemData);
-            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            var response = await _httpClient.PostAsync("Orders/AddOrderItem", content);
-
-            if (!response.IsSuccessStatusCode)
-            {
-                _NotyfService.Error("خطا در ارسال سفارش");
+                _notyfService.Error("خطا در ارسال سفارش");
             }
-
-            var result = await response.Content.ReadAsStringAsync();
-            _NotyfService.Success("محصول به سفارشات شما اضافه شد");
-            return Content(result);
+            else
+            {
+                _notyfService.Success("محصول به سفارشات شما اضافه شد");
+            }
+            var orders = await _orderService.GetLastOpenOrderItems(customerId);
+            return Content(orders.Count().ToString());
         }
         [HttpGet]
-        public async Task<IActionResult> RemoveOrderItem(int OrderId, int ProductId)
+        public async Task<IActionResult> RemoveOrderItem(int orderId, int productId)
         {
-            var orderItemData = new
+            var isSuccess = await _orderService.RemoveOrderItem(orderId, productId);
+            if (!isSuccess)
             {
-                orderId = OrderId,
-                productId = ProductId
-            };
-
-            var jsonData = JsonConvert.SerializeObject(orderItemData);
-            var content = new StringContent(jsonData, Encoding.UTF8, "application/json");
-
-            var request = new HttpRequestMessage(HttpMethod.Delete, "Orders/RemoveOrderItems")
+                _notyfService.Error("خطا در ارسال درخواست");
+            }
+            else
             {
-                Content = content
-            };
-
-            var response = await _httpClient.SendAsync(request);
-
-
-            if (!response.IsSuccessStatusCode)
-            {
-                _NotyfService.Error("خطا در ارسال درخواست");
+                _notyfService.Success("محصول از لیست سفارشات حذف شد");
             }
 
-            var result = await response.Content.ReadAsStringAsync();
-            _NotyfService.Success("محصول از لیست سفارشات حذف شد");
-            return RedirectToAction("Orders", new { CustomerId = 1 });
+            return RedirectToAction("Orders", new { customerId = 1 });
         }
     }
 }
