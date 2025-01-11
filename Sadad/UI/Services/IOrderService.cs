9b7d8599ec7b62fda66220c4d17fcdf5e111fb5d
Hamed Ziaei <h.zEEAEE@gmail.com>
2025-01-11 10:54:43 +0330

    [Fix] Add Interface and change Pattern

---
+++ Sadad/UI/Services/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using API.DTOs;
+
+namespace UI.Services
+{
+    public interface IOrderService
+    {
+        Task<List<ProductDto>> GetProducts();
+        Task<List<OrderItemDto>> GetLastOpenOrderItems(int customerId);
+        Task<List<OrderListGroupDto>> GetOrderGroupItems(int customerId);
+        Task<bool> CommitOrderAsync(int orderId);
+        Task<bool> AddOrderItem(int customerId, int productId);
+        Task<bool> RemoveOrderItem(int orderId, int productId);
+    }
+}
