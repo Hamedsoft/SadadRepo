e7462cbb04f633412dfb2e1db85fb943d7b90a44
Hamed Ziaei <h.zEEAEE@gmail.com>
2025-01-18 20:13:56 +0330

    [DB] Migration Test

---
+++ Sadad/Infrastructure/Migrations/20250118164142_InitialCreate.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orders",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Customer = table.Column<int>(type: "int", nullable: false),
+                    SubTotal = table.Column<double>(type: "float", nullable: false),
+                    Status = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orders", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "Nvarchar(256)", nullable: false),
+                    Rate = table.Column<int>(type: "int", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    ImageFileName = table.Column<string>(type: "Nvarchar(256)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OrderItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderItems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "ImageFileName", "Name", "Price", "Rate" },
+                values: new object[,]
+                {
+                    { 1, "1.jpg", "شیائومی مدل Redmi Note 13 Pro", 275000000.0, 4 },
+                    { 2, "2.jpg", "اپل مدل iPhone 13 CH 256", 575000000.0, 3 },
+                    { 3, "3.jpg", "اپل مدل iPhone 13 pro ZAA 128", 757000000.0, 5 },
+                    { 4, "4.jpg", "سامسونگ مدل Galaxy A15 128", 265000000.0, 4 },
+                    { 5, "5.jpg", "سامسونگ مدل Galaxy A35 128", 219000000.0, 3 },
+                    { 6, "6.jpg", "سامسونگ مدل Galaxy A05 256", 175000000.0, 1 },
+                    { 7, "7.jpg", "نوکیا مدل 1100", 75000000.0, 5 },
+                    { 8, "8.jpg", "سامسونگ مدل Galaxy A55 512", 143000000.0, 2 },
+                    { 9, "9.jpg", "سامسونگ مدل Galaxy S24 512", 331000000.0, 4 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_OrderId",
+                table: "OrderItems",
+                column: "OrderId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderItems_ProductId",
+                table: "OrderItems",
+                column: "ProductId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderItems");
+
+            migrationBuilder.DropTable(
+                name: "Orders");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
