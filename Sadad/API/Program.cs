435918885dff2747a1162a292234fb9b82f53736
Hamed Ziaei <h.zEEAEE@gmail.com>
2025-01-18 16:06:52 +0330

    [add] Add Ocelot

--- Sadad/API/Program.cs
+++ Sadad/API/Program.cs
@@ -1,15 +1,17 @@
-using Application.Queries.Orders.GetAllOrders;
+﻿using Application.Queries.Orders.GetAllOrders;
 using Autofac.Extensions.DependencyInjection;
 using Infrastructure.Extensions;
 using MediatR;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Change IoC to Autofac
+// Autofac
 builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-// Register Application & Infrastructure Services
+// connection String
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddInfrastructure(connectionString);
 
@@ -22,6 +24,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// ocelot
+//builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+//builder.Services.AddOcelot();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -29,10 +35,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>{c.SwaggerEndpoint("/swagger/v1/swagger.json", "Sadad API");});
 }
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+//app.UseOcelot().Wait();
+
 app.Run();
